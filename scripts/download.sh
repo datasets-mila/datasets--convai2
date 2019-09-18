fa78e41602905e42457deb038bef9575274d53e9
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2019-09-18 11:57:24 -0400

    Add download script and md5sums

---
+++ scripts/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is meant to be used with the command 'datalad run'
+
+git-annex addurl --raw --file convai2_fix_723.tgz http://parl.ai/downloads/convai2/convai2_fix_723.tgz
+
